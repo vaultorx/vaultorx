dfc12de3f351e842b721ab21ff30137f87f6f6c6
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-11-01 18:37:36 +0100

    chore: remove prisma options

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,18 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-    datasources: { db: { url: process.env.DATABASE_URL } },
-    // 👇 add this
-    transactionOptions: {
-      maxWait: 5000,
-      timeout: 10000,
-      isolationLevel: "ReadCommitted",
-    },
-  });
+const prisma = globalForPrisma.prisma || new PrismaClient({ log: ["query"] });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
