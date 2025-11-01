5e6342c9a54a07db2086bd8362652d0a4dae846d
Adebayo Anuoluwa Success <adebayo.anuoluwa02@gmail.com>
2025-11-01 18:31:40 +0100

    fix: change the prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,7 +4,18 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma || new PrismaClient({ log: ["query"] });
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ["query"],
+    datasources: { db: { url: process.env.DATABASE_URL } },
+    // 👇 add this
+    transactionOptions: {
+      maxWait: 5000,
+      timeout: 10000,
+      isolationLevel: "ReadCommitted",
+    },
+  });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
